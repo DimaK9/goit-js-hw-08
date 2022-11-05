e95a2fa9d182e6db1e5d0133552f7602be1526ea
DimaK9 <103372116+DimaK9@users.noreply.github.com>
2022-11-05 19:07:59 +0000

    Deploying to gh-pages from @ DimaK9/goit-js-hw-08@38c4e65550a9efa61dd29515e70f4d1a1b726540 🚀

---
+++ 03-feedback.2e42c52b.js
@@ -0,0 +1,2 @@
+const e={form:document.querySelector(".feedback-form"),textarea:document.querySelector(".message")};console.log(e.form);console.log(5);
+//# sourceMappingURL=03-feedback.2e42c52b.js.map

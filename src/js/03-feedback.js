22545ba04aceb71ce136a6ac5afb37ad6e63b141
DimaK9 <103372116+DimaK9@users.noreply.github.com>
2022-11-06 14:39:54 +0200

    1

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,16 +1,24 @@
+import throttle from 'lodash.throttle';
+
+const STORAGE_KEY ='feedback-msg';
+
 const refs = {
     form: document.querySelector('.feedback-form'),
     input:document.querySelector('input'),
     textarea: document.querySelector('textarea'),
 };
-console.log(refs);
+console.log(refs.form);
 
 refs.form.addEventListener('submit', onFormSubmit);
-refs.input.addEventListener('input', onInputChange);
-refs.textarea.addEventListener('input', onTextareaChange);
+refs.input.addEventListener('input', throttle(onInputChange, 500));
+refs.textarea.addEventListener('input', throttle(onTextareaChange, 500));
+
 
 function onFormSubmit(evt) {
+    evt.preventDefault();
 
+    evt.currentTarget.reset();
+    localStorage.removeItem(STORAGE_KEY);
 }
 
 function onInputChange(evt) {
@@ -20,5 +28,5 @@ function onInputChange(evt) {
 function onTextareaChange(evt) {
 const message = evt.currentTarget.value;
 
-localStorage.setItem('feedback-msg', message);
+localStorage.setItem(STORAGE_KEY, message);
 }

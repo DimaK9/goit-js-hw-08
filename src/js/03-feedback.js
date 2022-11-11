cec42b3d1a2e2a85697f2ea336d23d915e007a5b
DimaK9 <103372116+DimaK9@users.noreply.github.com>
2022-11-11 19:28:56 +0200

    Виправив помилку

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,51 +1,49 @@
-import Throttle from 'lodash.throttle';
+import throttle from 'lodash.throttle';
 
 const refs = {
-    form: document.querySelector('.feedback-form'),
-    textarea: document.querySelector('.feedback-form textarea'),
-    input: document.querySelector('.feedback-form input'),
-  };
-  
-  const STORAGE_KEY = 'feedback-form-state';
-  let dataForm = {};
-  
-  refs.form.addEventListener('submit', onFormSubmit);
-  refs.form.addEventListener('input', throttle(onTextareaInput, 500));
+  form: document.querySelector('.feedback-form'),
+  textarea: document.querySelector('.feedback-form textarea'),
+  input: document.querySelector('.feedback-form input'),
+};
 
+const STORAGE_KEY = 'feedback-form-state';
+let dataForm = {};
+
+refs.form.addEventListener('submit', onFormSubmit);
+refs.form.addEventListener('input', throttle(onTextareaInput, 500));
 
 function onFormSubmit(event) {
-    event.preventDefault();
+  event.preventDefault();
 
-     console.log(dataForm);
-    event.currentTarget.reset();
-    localStorage.removeItem(STORAGE_KEY);
+   console.log(dataForm);
+  event.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
 
-    dataForm[refs.input.name] = '';
-    dataForm[refs.textarea.name] = '';
-  }
-  
-  function onTextareaInput(event) {
-    dataForm[event.target.name] = event.target.value;
+   dataForm[refs.input.name] = '';
+  dataForm[refs.textarea.name] = '';
+}
 
-    localStorage.setItem(STORAGE_KEY, JSON.stringify(dataForm));
-  }
-  
-  populateTextarea();
-  
-  function populateTextarea() {
-    const savedMessage = localStorage.getItem(STORAGE_KEY);
-    const parsedMessage = JSON.parse(savedMessage);
+function onTextareaInput(event) {
+  dataForm[event.target.name] = event.target.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(dataForm));
+}
 
-    if (savedMessage) {
-      if (parsedMessage.email) {
-        refs.input.value = parsedMessage.email;
-        dataForm[refs.input.name] = parsedMessage.email;
-      }
+populateTextarea();
 
-      if (parsedMessage.message) {
-        refs.textarea.value = parsedMessage.message;
-        dataForm[refs.textarea.name] = parsedMessage.message;
-      }
+function populateTextarea() {
+  const savedMessage = localStorage.getItem(STORAGE_KEY);
+  const parsedMessage = JSON.parse(savedMessage);
+
+  if (savedMessage) {
+    if (parsedMessage.email) {
+      refs.input.value = parsedMessage.email;
+      dataForm[refs.input.name] = parsedMessage.email;
+    }
+    
+    if (parsedMessage.message) {
+      refs.textarea.value = parsedMessage.message;
+      dataForm[refs.textarea.name] = parsedMessage.message;
     }
   }
+}
   
